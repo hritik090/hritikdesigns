56a5ff714fa8eab3ad5babb7b4f75e950e68bc48
hritik090 <hritik090s@gmail.com>
2026-01-22 12:29:52 +0530

    paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Welcome to My Portfolio</h1>
+      <p>This is the homepage of my personal portfolio website.</p>
     </main>
   );
 }
