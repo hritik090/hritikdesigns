ff5bc3427f80319ed99222d09debe52446c56bf8
hritik090 <hritik090s@gmail.com>
2026-01-22 13:56:56 +0530

    heading and font

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the homepage of my personal portfolio website.</p>
+    <main style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}>
+      <h1 className="heading">Hritik sharma Coming soon</h1>
     </main>
   );
 }
